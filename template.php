c4af6dca71a4a0b48134cdc78ccad7930ec6e100
Isaac J <duckhunterjr@gmail.com>
2021-10-10 15:47:31 -0700

    add mising php include code

--- template.php
+++ template.php
@@ -1,6 +1,10 @@
+<?php include 'includes/header.php'; ?>
+
 <section>
     <h2 class="pageID">Welcome!</h2>
     <p>
         Model to create additional pages.
     </p>
 </section>
+
+<?php include 'includes/footer.php'; ?>
