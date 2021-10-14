3131efb7a56470aba203f331ab461233eea688d1
Isaac J <duckhunterjr@gmail.com>
2021-10-13 23:52:35 -0700

    add php files

--- contactme.php
+++ contactme.php
@@ -2,9 +2,24 @@
 
 <section>
     <h2 class="pageID">Contact Me</h2>
-    <p>
-        Coming soon! Page will soon include link to contact me page.
-    </p>
+    <?php
+    /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+    include 'includes/contact_include.php'; #site keys & code here
+
+    $toAddress = "jimenez.isaac.jr@gmail.com";  //place your/your client's email address here
+    $toName = "Isaac Jimenez"; //place your client's name here
+    $website = "Contact test";  //place NAME of your client's website
+
+    echo loadContact('simple.php'); #demonstrates a simple contact form
+    //echo loadContact('multiple.php');#demonstrates multiple form elements
+
+    ?>
 </section>
 
 <?php include 'includes/footer.php'; ?>
