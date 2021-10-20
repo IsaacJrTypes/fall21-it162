05f2c14a7a1b60c26d6a043ed33273292c71a01f
Isaac J <duckhunterjr@gmail.com>
2021-10-20 13:54:10 -0700

    add php dynamic timezone/year to footer

--- includes/portal-config.php
+++ includes/portal-config.php
@@ -9,6 +9,9 @@ Used to store all of our WEB120 configuration information
 //prevents data from being sent early
 ob_start();
 
+//this helps us avoid PHP date errors:
+date_default_timezone_set('America/Los_Angeles');
+
 
 //echo basename($_SERVER['PHP_SELF']);
 
