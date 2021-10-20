3f5ee8b460bb7382f01ee4c4b9b56a4cd12c091b
Isaac J <duckhunterjr@gmail.com>
2021-10-20 14:21:33 -0700

    removes h2 to header and nav comments

--- includes/header.php
+++ includes/header.php
@@ -24,12 +24,6 @@
             <nav>
                 <ul class="topnav" id="myTopnav">
                     <?=makeLinks($nav1)?>
-                    <!-- <li><a href="index.php" class="selected">Welcome</a></li>
-                    <li><a href="big/index.php">Big</a></li>
-                    <li><a href="aia.php">AIA</a></li>
-                    <li><a href="flowchart.php">Flowchart/Layout</a></li>
-                    <li><a href="fp/index.php">Final Project</a></li>
-                    <li><a href="contactme.php">Contact Isaac</a></li>-->
                     <li class="icon">
                         <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
                     </li> 
