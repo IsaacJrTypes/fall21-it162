ca12ca931d34c96758f0949f106471df75160326
Isaac J <duckhunterjr@gmail.com>
2021-11-30 22:28:56 -0800

    add js nav menu

---
+++ fp/js/navScript.js
@@ -0,0 +1,11 @@
+(function ($) {
+    $(document).ready(function () {
+
+        $("#cssmenu").menumaker({
+            title: "Menu",
+            breakpoint: 967,
+            format: "multitoggle"
+        });
+
+    });
+})(jQuery);
