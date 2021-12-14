fc1a8aa77cd7dbd861c53fd44c8a2d082271e0ea
Isaac J <duckhunterjr@gmail.com>
2021-12-13 22:19:55 -0800

    add gallery files

---
+++ fp/gallery.php
@@ -0,0 +1,71 @@
+<?php include 'includes/header.php'; ?>
+      <!-- Start Grid Gallery -->
+      <section id="Gallery">
+        <h2>Gallery</h2>
+        <div class="grid-container">
+          <div class="grid-item">
+            <figure class="grid-item-frame">
+              <img
+                src="https://images.unsplash.com/photo-1638261127771-4d0201c4274e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=8"
+                alt="senic view of mountain"
+              />
+              <figcaption>Caption 1 Here</figcaption>
+            </figure>
+          </div>
+          <div class="grid-item">
+            <figure class="grid-item-frame">
+              <img
+                src="https://images.unsplash.com/photo-1524815643232-784d5cea8664?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                alt="girl staring into sunrise"
+              />
+              <figcaption>Caption 2 Here</figcaption>
+            </figure>
+          </div>
+
+          <div class="grid-item">
+            <figure class="grid-item-frame">
+              <img
+                src="https://images.unsplash.com/photo-1492500318351-274a118407c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                alt="hiker stearing at mountains"
+              />
+              <figcaption>Caption 3 Here</figcaption>
+            </figure>
+          </div>
+          <div class="grid-item">
+            <figure class="grid-item-frame">
+              <img
+                src="https://media.istockphoto.com/photos/nova-scotia-duck-tolling-retriever-dog-sits-in-woods-picture-id619770286?b=1&k=20&m=619770286&s=170667a&w=0&h=SIK7BB4ZRNGeY33CIJIkks3MNWjM9NE-UDv37CfpzbI="
+              />
+              <figcaption>Caption 4 Here</figcaption>
+            </figure>
+          </div>
+          <div class="grid-item">
+            <figure class="grid-item-frame">
+              <img
+                src="https://images.unsplash.com/photo-1543421481-67fcbb18a3b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              />
+              <figcaption>Caption 5 Here</figcaption>
+            </figure>
+          </div>
+          <div class="grid-item">
+            <figure class="grid-item-frame">
+              <img
+                class="lrg"
+                src="https://images.unsplash.com/photo-1502982899975-893c9cf39028?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              />
+              <figcaption>Caption 6 Here</figcaption>
+            </figure>
+          </div>
+          <div class="grid-item">
+            <figure class="grid-item-frame">
+              <img
+                class="lrg"
+                src="https://images.unsplash.com/photo-1508602636771-613ba0112c30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              />
+              <figcaption>Caption 7 Here</figcaption>
+            </figure>
+          </div>
+        </div>
+      </section>
+      <!-- End Grid Gallery -->
+<?php include 'includes/footer.php'; ?>
