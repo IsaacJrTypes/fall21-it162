12d606a52d43eebc84b7a4593db3ce929d884fb6
Isaac J <duckhunterjr@gmail.com>
2021-12-13 22:04:50 -0800

    add shop.php file

---
+++ fp/shop.php
@@ -0,0 +1,161 @@
+<?php include 'includes/header.php'; ?>
+  <!-- Start Shopping Page -->
+  <section>
+        <h2 id="shop">Shop</h2>
+        <div class="grid-container shop">
+          <div class="container-card item-shop">
+            <div class="card-shop one">
+              <img
+                src="https://images.unsplash.com/photo-1638261127771-4d0201c4274e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=8"
+              />
+              <div class="header-item-shop">
+                <h3>
+                  Title 1
+                  <span class="price">$30</span>
+                </h3>
+              </div>
+              <div class="content-item-shop">
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Assumenda incidunt natus quod maiores cum animi eligendi eum
+                  accusantium ab dolores velit, neque possimus explicabo
+                  veritatis eaque id, quidem porro distinctio!
+                </p>
+              </div>
+              <a class="btn-shop" href="#">Buy</a>
+            </div>
+          </div>
+          <div class="container-card item-shop">
+            <div class="card-shop two">
+              <img
+                src="https://images.unsplash.com/photo-1492500318351-274a118407c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              />
+              <div class="header-item-shop">
+                <h3>
+                  Title 2
+                  <span class="price">$30</span>
+                </h3>
+              </div>
+              <div class="content-item-shop">
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Assumenda incidunt natus quod maiores cum animi eligendi eum
+                  accusantium ab dolores velit, neque possimus explicabo
+                  veritatis eaque id, quidem porro distinctio!
+                </p>
+              </div>
+              <a class="btn-shop" href="#">Buy</a>
+            </div>
+          </div>
+          <div class="container-card item-shop">
+            <div class="card-shop three">
+              <img
+                src="https://images.unsplash.com/photo-1502982899975-893c9cf39028?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              />
+              <div class="header-item-shop">
+                <h3>
+                  Title 3
+                  <span class="price">$30</span>
+                </h3>
+              </div>
+              <div class="content-item-shop">
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Assumenda incidunt natus quod maiores cum animi eligendi eum
+                  accusantium ab dolores velit, neque possimus explicabo
+                  veritatis eaque id, quidem porro distinctio!
+                </p>
+              </div>
+              <a class="btn-shop" href="#">Buy</a>
+            </div>
+          </div>
+          <div class="container-card item-shop">
+            <div class="card-shop four">
+              <img
+                src="https://images.unsplash.com/photo-1524815643232-784d5cea8664?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              />
+              <div class="header-item-shop">
+                <h3>
+                  Title 4
+                  <span class="price">$30</span>
+                </h3>
+              </div>
+              <div class="content-item-shop">
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Assumenda incidunt natus quod maiores cum animi eligendi eum
+                  accusantium ab dolores velit, neque possimus explicabo
+                  veritatis eaque id, quidem porro distinctio!
+                </p>
+              </div>
+              <a class="btn-shop" href="#">Buy</a>
+            </div>
+          </div>
+          <div class="container-card item-shop">
+            <div class="card-shop five">
+              <img
+                src="https://images.unsplash.com/photo-1543421481-67fcbb18a3b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              />
+              <div class="header-item-shop">
+                <h3>
+                  Title 5
+                  <span class="price">$30</span>
+                </h3>
+              </div>
+              <div class="content-item-shop">
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Assumenda incidunt natus quod maiores cum animi eligendi eum
+                  accusantium ab dolores velit, neque possimus explicabo
+                  veritatis eaque id, quidem porro distinctio!
+                </p>
+              </div>
+              <a class="btn-shop" href="#">Buy</a>
+            </div>
+          </div>
+          <div class="container-card item-shop">
+            <div class="card-shop six">
+              <img src="images/wanderingWalk.jpg" />
+              <div class="header-item-shop">
+                <h3>
+                  Title 6
+                  <span class="price">$30</span>
+                </h3>
+              </div>
+              <div class="content-item-shop">
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Assumenda incidunt natus quod maiores cum animi eligendi eum
+                </p>
+              </div>
+              <a class="btn-shop" href="#">Buy</a>
+            </div>
+          </div>
+          <div class="container-card item-shop">
+            <div class="card-shop six">
+              <div class="header-info-shop">
+                <h3>Additional Information</h3>
+              </div>
+              <div class="feature-list shop">
+                <ul>
+                  <li>
+                    <p>
+                      Need help picking out a frame? (link here)
+                    </p>
+                  </li>
+                  <li><p>Best hiking destinations (link here)</p></li>
+                  <li>
+                    <p>
+                      How to use natural lighting when taking your own photos
+                      (link here)
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- End Shopping Page -->
+      <?php include 'includes/footer.php'; ?>
