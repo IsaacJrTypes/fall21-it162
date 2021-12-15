d4c20060200aa1aff6e7718021f7a1725c1e4b64
Isaac J <duckhunterjr@gmail.com>
2021-12-14 16:13:37 -0800

    add google tag manager

--- fp/includes/header.php
+++ fp/includes/header.php
@@ -2,6 +2,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PG7XCDP');</script>
+<!-- End Google Tag Manager -->
     <title><?= $title ?></title>
     <meta charset="UTF-8" />
     <meta name="robots" content="noindex,nofollow" />
@@ -31,6 +38,10 @@
     
   </head>
   <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PG7XCDP"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <main>
       <!-- Start Header -->
       <header>
