71a6572a24f1c0ca0245f0b2715a1e41142ed542
Isaac J <duckhunterjr@gmail.com>
2021-12-14 17:21:39 -0800

    adds scroll button

--- fp/includes/header.php
+++ fp/includes/header.php
@@ -29,6 +29,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <link rel="stylesheet" href="css/shop.css" />
     <link rel="stylesheet" href="css/blog.css" />
     <link rel="stylesheet" href="css/contact.css" />
+    <link rel="stylesheet" href="css/button.css" />
    
     <!-- jQuary-->
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,9 @@
       </section>
       <footer>
           <p>
-              <small>&copy; <?=date('Y')?> by <a href="contact.php">Isaac Jimenez</a>, All Rights
+              <small>&copy; <?= date(
+                  'Y'
+              ) ?> by <a href="contact.php">Isaac Jimenez</a>, All Rights
                   Reserved ~
                   <a id="html-checker" href="#" target="_blank">Check HTML</a> ~
                   <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">Check CSS</a></small>
@@ -11,6 +13,9 @@
       <!-- END Footer -->
       </main>
       <!-- END WRAPPER -->
+
+      <button onclick="topFunction()" id="myBtn" title="Go to top">^</button>
+    <script src="js/button.js"></script>
       <!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise -->
       <script src="js/nav.js"></script>
       </body>
