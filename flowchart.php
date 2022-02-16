32cd280cb54ad2787c06d7b7daf37c16fce7488d
Isaac J <duckhunterjr@gmail.com>
2022-02-16 15:55:15 -0800

    add minor capitalization

--- flowchart.php
+++ flowchart.php
@@ -1,10 +1,10 @@
 <?php include 'includes/header.php'; ?>
 
     <p>
-        Wireframe and flowchart: <a href="https://docs.google.com/document/d/1Rjrv5JZO3TpbVqEs_H2YdJ9JMh1ZVURxX2189b_Tu94/edit?usp=sharing" target="_blank" title="Link to google doc">Opens in new Tab</a>
+        Wireframe and flowchart: <a href="https://docs.google.com/document/d/1Rjrv5JZO3TpbVqEs_H2YdJ9JMh1ZVURxX2189b_Tu94/edit?usp=sharing" target="_blank" title="Link to google doc">Opens in new tab</a>
     </p>
     <p>
-        Client Industry Research: <a href="https://docs.google.com/document/d/1T-SqANKwXhBjM3hcgHtAaY-zOovT4Bj6wJrKVFLUcuo/edit?usp=sharing" target="_blank" title="Link to google doc">Opens in new Tab</a>
+        Client Industry Research: <a href="https://docs.google.com/document/d/1T-SqANKwXhBjM3hcgHtAaY-zOovT4Bj6wJrKVFLUcuo/edit?usp=sharing" target="_blank" title="Link to google doc">Opens in new tab</a>
     </p>
 
 
